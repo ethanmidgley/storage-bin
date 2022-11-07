db17f6a5b6a62064f0487f2893dedd95266d0de3
ethanmidgley <ethan.c.midgley@gmail.com>
2022-11-07 03:41:04 +0000

    Added backup 404 message

--- app/routes/item/$id.tsx
+++ app/routes/item/$id.tsx
@@ -1,10 +1,14 @@
-import { Response } from "@remix-run/node";
+import { redirect, Response } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { getFile } from "~/utils/getFile.server";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.id, "File id");
-  const data = await getFile(params.id);
-  return new Response(data);
+  try {
+    const data = await getFile(params.id);
+    return new Response(data);
+  } catch (err) {
+    return redirect("/404");
+  }
 };
