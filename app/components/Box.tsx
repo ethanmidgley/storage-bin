72768545fb746709777e580e88e63a5e6fefa956
ethanmidgley <ethan.c.midgley@gmail.com>
2022-11-07 03:41:26 +0000

    Updated components

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -5,7 +5,7 @@ interface BoxProps {
 
 export const Box: React.FC<BoxProps> = ({ children, className }) => (
   <div
-    className={`text-gray-900 dark:text-white p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ${className}`}
+    className={`text-gray-900 dark:text-white p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ${className}`}
   >
     {children}
   </div>
