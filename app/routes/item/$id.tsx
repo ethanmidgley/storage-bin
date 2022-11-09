b25c3650ff9bfe8402c5083973b6cac62fab9db1
ethanmidgley <ethan.c.midgley@gmail.com>
2022-11-09 03:09:31 +0000

    Refactored files management util

--- app/routes/item/$id.tsx
+++ app/routes/item/$id.tsx
@@ -1,12 +1,12 @@
 import { redirect, Response } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 import invariant from "tiny-invariant";
-import { getFile } from "~/utils/getFile.server";
+import files from "~/utils/files.server";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.id, "File id");
   try {
-    const data = await getFile(params.id);
+    const data = await files.get(params.id);
     return new Response(data);
   } catch (err) {
     return redirect("/404");
